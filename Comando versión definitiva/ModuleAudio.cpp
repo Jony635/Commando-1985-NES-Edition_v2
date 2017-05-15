0f1dbf4b02c4a920990439848ff16e109fe97d08
Jony635 <jony635molina@gmail.com>
2017-05-15 13:22:30 +0200

    Module Audio improved

--- Comando versión definitiva/ModuleAudio.cpp
+++ Comando versión definitiva/ModuleAudio.cpp
@@ -5,6 +5,7 @@
 #include "ModuleFadeToBlack.h"
 #include "ModuleAudio.h"
 #include "SDL_mixer\include\SDL_mixer.h"
+#include "SDL\include\SDL.h"
 
 
 
@@ -18,13 +19,34 @@ ModuleAudio::~ModuleAudio()
 // Load assets
 bool ModuleAudio::Start()
 {
-	/*App->audio->Enable();
-	LOG("Loading general audio");
-	Mix_Init(MIX_INIT_OGG);
+	LOG("Loading Audio Mixer");
+	bool ret = true;
+	SDL_Init(0);
 
-	Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048);*/
+	if (SDL_InitSubSystem(SDL_INIT_AUDIO) < 0)
+	{
+		LOG("SDL_INIT_AUDIO could not initialize! SDL_Error: %s\n", SDL_GetError());
+		ret = false;
+	}
 
-	return true;
+	// load support for the OGG format
+	int flags = MIX_INIT_OGG;
+	int init = Mix_Init(flags);
+
+	if ((init & flags) != flags)
+	{
+		LOG("Could not initialize Mixer lib. Mix_Init: %s", Mix_GetError());
+		ret = false;
+	}
+
+	//Initialize SDL_mixer
+	if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0)
+	{
+		LOG("SDL_mixer could not initialize! SDL_mixer Error: %s\n", Mix_GetError());
+		ret = false;
+	}
+
+	return ret;
 }
 
 
@@ -69,19 +91,26 @@ bool ModuleAudio::PlaySound(const char* path)
 
 bool ModuleAudio::Stop()
 {
-	/*if (App->stop_music) {
+	if (App->stop_music) {
 		Mix_PauseMusic();
-	}*/
+	}
 	return true;
 }
 
 
 bool ModuleAudio::CleanUp()
 {
-	/*LOG("Quitting SDL input event subsystem");
-	Stop();
-	Mix_FreeMusic(soundtrack);
-	Mix_Pause(-1);
-	Mix_Quit();*/
+	LOG("Freeing sound FX, closing Mixer and Audio subsystem");
+	if (soundtrack != NULL)
+	{
+		Mix_FreeMusic(soundtrack);
+	}
+	if (sounfeffect1 != nullptr)
+	{
+		Mix_FreeChunk(sounfeffect1);
+	}
+	Mix_CloseAudio();
+	Mix_Quit();
+	SDL_QuitSubSystem(SDL_INIT_AUDIO);
 	return true;
 }
