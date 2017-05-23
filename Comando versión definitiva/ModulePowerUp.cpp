f59965148fd45bc3bb804669cab107891302100e
Jony635 <jony635molina@gmail.com>
2017-05-23 22:34:05 +0200

    PowerUp animation speeds corrected

--- Comando versión definitiva/ModulePowerUp.cpp
+++ Comando versión definitiva/ModulePowerUp.cpp
@@ -18,46 +18,55 @@ ModulePowerUp::ModulePowerUp()
 	binocular.PushBack({ 179,70,16,16 });
 	binocular.PushBack({ 155,93,16,16 });
 	binocular.PushBack({ 178,93,16,16 });
+	binocular.speed = 0.08f;
 
 	bulletproof_vest.PushBack({ 54,71,14,14 });
 	bulletproof_vest.PushBack({ 77,71,14,14 });
 	bulletproof_vest.PushBack({ 54,93,14,14 });
 	bulletproof_vest.PushBack({ 77,93,14,14 });
+	bulletproof_vest.speed = 0.08f;
 
 	gasoline.PushBack({ 151,27,12,15 });
 	gasoline.PushBack({ 175,27,12,15 });
 	gasoline.PushBack({ 152,50,12,15 });
 	gasoline.PushBack({ 176,50,12,15 });
+	gasoline.speed = 0.08f;
 
 	grenadex4.PushBack({ 99,76,23,15 });
 	grenadex4.PushBack({ 125,76,23,15 });
 	grenadex4.PushBack({ 99,93,23,15 });
 	grenadex4.PushBack({ 125,93,23,15 });
+	grenadex4.speed = 0.08f;
 
 	grenadex5.PushBack({ 126,3,21,16 });
 	grenadex5.PushBack({ 102,3,21,16 });	
 	grenadex5.PushBack({ 150,3,21,16 });
 	grenadex5.PushBack({ 175,4,21,16 });
+	grenadex5.speed = 0.08f;
 
 	medal.PushBack({ 101,27,16,10 });
 	medal.PushBack({ 124,27,16,10 });
 	medal.PushBack({ 101,55,16,10 });
 	medal.PushBack({ 124,54,16,10 });
+	medal.speed = 0.08f;
 
 	medal_of_honor.PushBack({ 35,0,10,16 });
 	medal_of_honor.PushBack({ 47,0,10,16 });
 	medal_of_honor.PushBack({ 59,0,10,16 });
 	medal_of_honor.PushBack({ 71,0,10,16 });
+	medal_of_honor.speed = 0.08f;
 
 	barrel.PushBack({ 203,73,12,16 });
 	barrel.PushBack({ 228,73,12,16 });
 	barrel.PushBack({ 204,96,12,16 });
 	barrel.PushBack({ 228,96,12,16 });
+	barrel.speed = 0.08f;
 
 	bag.PushBack({ 6,23,12,15 });
 	bag.PushBack({ 32,23,12,15 });
 	bag.PushBack({ 6,46,12,15 });
 	bag.PushBack({ 33,47,12,15 });
+	bag.speed = 0.08f;
 }
 
 ModulePowerUp::~ModulePowerUp()
